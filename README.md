40646b4ab539cce567f83b8524bc68c3033d6113
Smith Jesko <smithjesko@gmail.com>
2019-09-16 20:58:06 -0500

    cli args

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # py-port-scanner
+
+#### Ports
+21      TCP,UDP     FTP
+22      TCP,UDP     SSH
+80      TCP         HTTP
--- port-scanner.py
+++ port-scanner.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 
+import argparse
 import socket
 
+parser = argparse.ArgumentParser(description='Simple port scanner utility written in Python 3.')
+parser.add_argument('-ip', '--IP', default='192.168.1.1', type=str, metavar= '', help='IP address to scan')
+# parser.add_argument('-p', '--port', default=22, type=int, metavar='', help='Port to scan')
+scanGroup = parser.add_mutually_exclusive_group()
+scanGroup.add_argument('-f', '--full', action='store_true')
+scanGroup.add_argument('-ftp', '--FTP', action='store_true')
+scanGroup.add_argument('-ssh', '--SSH', action='store_true')
+scanGroup.add_argument('-http', '--HTTP', action='store_true')
+args = parser.parse_args()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
+# s.settimeout(5)
 
-host = input("IP Address: ")
-port = int(input("Port: "))
+host = args.IP
+ports = {
+  'FTP': 21,
+  'SSH': 22,
+  'HTTP': 80
+}
 
-
-def portScanner(port):
-  if s.connect_ex((host, port)):
-    print('The port is closed.')
-  else:
-    print('The port is open.')
+def portScanner():
+  for scan, port in ports.items():
+    if s.connect_ex((host, port)):
+      print('{} {} - Closed'.format(scan, port))
+    else:
+      print('{} {} - Open'.format(scan, port))
 
 if __name__ == "__main__":
-  portScanner(port)
+  portScanner()
