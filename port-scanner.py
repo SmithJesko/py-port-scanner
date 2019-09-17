70600da5edb080bf23ee0ffa6a5c0a04a9e0337b
Smith Jesko <smithjesko@gmail.com>
2019-09-16 20:07:56 -0500

    basic scanner

--- port-scanner.py
+++ port-scanner.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "192.168.1.1"
+port = 22
+
+def portScanner(port):
+  if s.connect_ex((host, port)):
+    print('The port is closed.')
+  else:
+    print('The port is open.')
+
+if __name__ == "__main__":
+  portScanner(port)
