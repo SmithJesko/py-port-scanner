8cd7df55da141c28d730e743b796753c41fe0676
Smith Jesko <smithjesko@gmail.com>
2019-09-16 20:12:50 -0500

    user input and timeout

--- port-scanner.py
+++ port-scanner.py
@@ -3,9 +3,11 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
 
-host = "192.168.1.1"
-port = 22
+host = input("IP Address: ")
+port = int(input("Port: "))
+
 
 def portScanner(port):
   if s.connect_ex((host, port)):
